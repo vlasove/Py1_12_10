4e39ee930e03da519f2cb20a4e1bf13cf58bdb06
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-10-12 12:03:02 +0300

    init

---
+++ Lec1/main.py
@@ -0,0 +1,20 @@
+# Тема1.Стандартный ввод-вывод
+# Вывод на консоль или команда print()
+
+print("Hello world") # + '\n'
+print("New Python program")
+
+# А что если поместить данные в переменную?
+message = "Text in Message"
+print(message)
+
+
+# А что если хочется распечатать несколько фраз в одну строку?
+word1 = "Hello"
+word2 = "From"
+word3 = "Amount"
+
+print(word1, word2, word3)
+
+# А что если хочу кириллицу?
+print("Кириллица")
