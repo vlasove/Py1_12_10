f00ffab3999ff80e086791bf1d687fb43b5970ec
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-10-19 13:01:41 +0300

    added tasks

---
+++ Lec12/main3.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+queue = []
+
+for _ in range(n):
+    command = input()
+    if "Кто послед" in command:
+        #Кто последний? Я - Кузнецов.
+        lastname = ..... 
+        queue.append(lastname)
+    elif "Я только спро" in command:
+        #Я только спросить! Я - Иванова.
+        ....
+        lastname = ...
+        queue.insert(0, lastname)
+    else:
+        # "Следующий!"
+        if len(queue) > 0:
+            user = queue.pop(0)
+            print("Заходит", user + "!")
+        else:
+            print("В очереди никого нет.")
